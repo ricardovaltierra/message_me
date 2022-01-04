718fd3cfb609dd9892ee296ec5c60b1072a0aef0
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-03 21:51:50 -0600

    Add rooot and login routes

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#= ChatroomController
+#
+# Controller class used to handle
+# chatroom actions (so far root route)
+class ChatroomController < ApplicationController
+  def index
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#= SessionsController
+#
+# Controller class used to handle
+# basic app authentication
+class SessionsController < ApplicationController
+  def new
+  end
+end
